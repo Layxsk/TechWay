e6540ef28b65f75b241e8a40035c4fdad16f0b6a
Ítalo <igranjo@gmail.com>
2024-11-23 15:41:30 -0300

    Configuração da navegacao entre paginas via servidor e paths absolutos para recursos public

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 import userRoute from "./src/routes/auth.js";
-import viewRoute from "./src/routes/auth.js";
+import viewRoute from "./src/routes/views.js";
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "src", "public")));
